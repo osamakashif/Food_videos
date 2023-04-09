bce7890e3dc99e335065e77abc68d3c8d57a60ef
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2023-04-10 07:23:55 +1200

    [fix]: Positioning.
    Positioning of elements/components within Recipe Card fixed. Always in the same place regardless of other components being present or not.

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -6,13 +6,13 @@ import './links.css'
 
 export const RecipeCard = ({ name, youtube, website, cuisine, image }: Recipe) => {
     return (
-        <Card sx={{ width: 350, height: 250, marginTop: "2%", marginBottom: "2%" }}>
+        <Card sx={{ width: 350, height: 250, marginTop: "2%", marginBottom: "2%", position: "relative" }}>
             {image && <CardMedia
                 sx={{ height: 100 }}
                 image={image}
                 title="Recipe image"
             />}
-            <CardContent>
+            <CardContent sx={{ position: "absolute", top: "50%" }}>
                 {name && <Typography gutterBottom variant="h5" component="div">
                     {name}
                 </Typography>}
@@ -20,7 +20,7 @@ export const RecipeCard = ({ name, youtube, website, cuisine, image }: Recipe) =
                     {cuisine}
                 </Typography>}
             </CardContent>
-            <CardActions>
+            <CardActions sx={{ position: "absolute", bottom: 0 }}>
                 {website && <a className='website' href={website} target="_blank" rel="noreferrer">
                     <LanguageIcon />
                 </a>}
