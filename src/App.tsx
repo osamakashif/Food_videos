03f4c177cec27f78ec85887fd851221532c58a5f
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2023-04-09 22:54:17 +1200

    [feat]: Loading Recipes.
    Recipes get loaded and set in the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import { useState } from "react";
+import { Recipe } from "./interface/Recipe";
+import { readTSV } from "./helpers/file_reader";
+import { tsvToJSON } from "./helpers/JSON_convertors";
+import { recipeFromJSON } from "./helpers/recipe_creator";
 
 function App() {
+  const [recipes, setRecipes] = useState<Recipe[]>([])
+  readTSV(process.env.REACT_APP_DATA_LINK as string).then(res => {
+    let newRecipes: Recipe[] = []
+    let jsonDataArray = tsvToJSON(res)
+    for (let jsonData of jsonDataArray) {
+      newRecipes.push(recipeFromJSON(jsonData))
+    }
+      setRecipes(newRecipes)
+  })
   return (
     <div className="App">
       <header className="App-header">
